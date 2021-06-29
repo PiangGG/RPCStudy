89591a7c9e8cf0f988e8412fbb5531e96afdfdc1
PiangGG <974805475@qq.com>
2021-06-29 09:57:05 +0800

    init the game base

---
+++ Source/RPCGame/RPCGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPCGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPCGAME_API ARPCGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
